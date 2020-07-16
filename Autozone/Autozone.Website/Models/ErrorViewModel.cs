9dccd157295340de1ec45bbcfa09ab12b4eb4105
Dylan Beattie <dylan@dylanbeattie.net>
2020-07-16 10:40:39 +0100

    Added SSL certificate and HTTP1+2 hosting configuration to Autozone.Website

---
+++ Autozone/Autozone.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Autozone.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Autozone/Autozone.Website/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Autozone.Website {
+	public class Program {
+		public static void Main(string[] args) {
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder => {
+
+					webBuilder.ConfigureKestrel(options => {
+						// Setup a HTTP/2 endpoint without TLS.
+						options.ListenAnyIP(5000, o => o.Protocols = HttpProtocols.Http1AndHttp2);
+						options.UseSslIfFileExists(5001, @"D:\Dropbox\ursatile.pfx");
+					});
+
+
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+	public static class KestrelServerExtensions {
+		public static void UseSslIfFileExists(this KestrelServerOptions options, int port, string pfxFilePath, string password = null) {
+			if (File.Exists(pfxFilePath)) {
+				options.Listen(IPAddress.Any, port, listen => listen.UseHttps(pfxFilePath, password));
+			}
+		}
+	}
+
+}
