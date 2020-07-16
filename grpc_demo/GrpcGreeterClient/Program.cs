65c2d130c361d9816594fdf5d63a5af699402538
Dylan Beattie <dylan@dylanbeattie.net>
2020-07-16 15:12:09 +0100

    Added simple gRPC examples

---
+++ grpc_demo/GrpcGreeterClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeter;
+
+namespace GrpcGreeterClient {
+	class Program {
+		static async Task Main(string[] args) {
+            using var grpc = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(grpc);
+            var request = new HelloRequest { Name = "Everybody!" };
+            var response = await client.SayHelloAsync(request);
+            Console.WriteLine(response.Message);
+            Console.WriteLine("Press a key to exit...");
+            Console.ReadKey();
+		}
+	}
+}
