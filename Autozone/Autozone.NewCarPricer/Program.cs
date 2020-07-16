580ff13c0dea59512acbda69bb86f79a8143b06e
Dylan Beattie <dylan@dylanbeattie.net>
2020-07-16 15:47:14 +0100

    Added gRPC pricing calculator

--- Autozone/Autozone.NewCarPricer/Program.cs
+++ Autozone/Autozone.NewCarPricer/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using Autozone.Messages;
+using Autozone.PricingService;
+using EasyNetQ;
+using Grpc.Net.Client;
 
-namespace Autozone.NewCarPricer
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace Autozone.NewCarPricer {
+	class Program {
+		private const string AMQP =
+			"amqp://uzvpuvak:j1ulefFopg5uip8lNxKFXtwN7J03Dgqa@rattlesnake.rmq.cloudamqp.com/uzvpuvak";
+
+		private static IBus bus = RabbitHutch.CreateBus(AMQP);
+		private static Pricer.PricerClient pricerClient;
+
+		static void Main(string[] args) {
+			var grpc = GrpcChannel.ForAddress("https://workshop.ursatile.com:5003");
+			pricerClient = new Pricer.PricerClient(grpc);
+			Console.Write("Subscribing to messages!");
+			bus.Subscribe<NewCarListingMessage>($"newcarpricer", HandleNewCarListing);
+		}
+
+		private static void HandleNewCarListing(NewCarListingMessage message) {
+			Console.WriteLine("New car listing received");
+			var pricerRequest = new CarPriceRequest {
+				Colour = message.Colour,
+				Make = message.Make,
+				Model = message.Model,
+				Year = message.Year
+			};
+			var response = pricerClient.CalculatePrice(pricerRequest);
+			Console.WriteLine($"Car costs {response.Price} {response.Currency}");
+		}
+	}
 }
--- Autozone/Autozone.PricingService/Services/PricerService.cs
+++ Autozone/Autozone.PricingService/Services/PricerService.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
-namespace Autozone.PricingService {
+namespace Autozone.PricingService.Services {
 	public class PricerService : Pricer.PricerBase {
 		private readonly ILogger<PricerService> _logger;
 		public PricerService(ILogger<PricerService> logger) {
@@ -13,8 +11,12 @@ namespace Autozone.PricingService {
 		}
 
 		public override Task<CarPriceResponse> CalculatePrice(CarPriceRequest request, ServerCallContext context) {
+			var price = 1000;
+			if (request.Make.Equals("tesla", StringComparison.InvariantCultureIgnoreCase)) price = 50000;
+			if (request.Year < 1980) price = 10;
+
 			return Task.FromResult(new CarPriceResponse {
-				Price = 1000,
+				Price = price,
 				Currency = "GBP"
 			});
 		}
