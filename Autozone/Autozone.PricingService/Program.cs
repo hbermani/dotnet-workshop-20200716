1646f0e46c8468c1c1c2aa4bade4545bfa547c25
Dylan Beattie <dylan@dylanbeattie.net>
2020-07-16 15:24:43 +0100

    Configure gRPC server to use Ursatile certificates

--- Autozone/Autozone.PricingService/Program.cs
+++ Autozone/Autozone.PricingService/Program.cs
@@ -2,26 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
-namespace Autozone.PricingService
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+namespace Autozone.PricingService {
+	public class Program {
+		public static void Main(string[] args) {
+			CreateHostBuilder(args).Build().Run();
+		}
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+		// Additional configuration is required to successfully run gRPC on macOS.
+		// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder => {
+					webBuilder.ConfigureKestrel(options => {
+						// Setup a HTTP/2 endpoint without TLS.
+						options.ListenAnyIP(5002, o => o.Protocols = HttpProtocols.Http1AndHttp2);
+						options.UseSslIfFileExists(5003, @"D:\Dropbox\ursatile.pfx");
+					});
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+
+	public static class KestrelServerExtensions {
+		public static void UseSslIfFileExists(this KestrelServerOptions options, int port, string pfxFilePath, string password = null) {
+			if (File.Exists(pfxFilePath)) {
+				options.Listen(IPAddress.Any, port, listen => listen.UseHttps(pfxFilePath, password));
+			}
+		}
+	}
 }
